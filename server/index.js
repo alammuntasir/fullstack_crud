650307c206427325682288a8a93a35fb3c6b2971
alammuntasir <alammuntasir01@gmail.com>
2025-09-07 02:27:28 +0600

    route define

--- server/index.js
+++ server/index.js
@@ -18,6 +18,16 @@ mongoose
 
 //   route define
 
+app.post("/addtodo", (req, res) => {
+  return res
+    .status(201)
+    .json({ success: true, message: "todo created successfull" });
+});
+
+app.get("/gettodo", (req,res)=>{
+    return res.status(200).json({ success:true, message: "todo fetch successfully"});
+});
+
 
 
 app.listen(process.env.PORT, () => {
