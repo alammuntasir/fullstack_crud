c19170e4fe10cd15fe02daa9d72c672d73281121
alammuntasir <alammuntasir01@gmail.com>
2025-09-07 01:53:29 +0600

    Mongo DB conneted

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const { default: mongoose } = require("mongoose");
+
+const crbolse = process.env.DB_URL;
+
+// database connectionn
+
+mongoose
+  .connect(crbolse)
+  .then(() => {
+    console.log("db Connected");
+  })
+  .catch((err) => {
+    console.log(err.message || err);
+  });
+
+//   route define
+
+
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running port number ${process.env.PORT}`);
