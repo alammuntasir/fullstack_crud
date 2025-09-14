d33c6ac00442c392f795a9474e1cf1ab0f125e38
alammuntasir <alammuntasir01@gmail.com>
2025-09-15 03:09:43 +0600

    Update and delete done

---
+++ server/model/todoSchema.js
@@ -0,0 +1,15 @@
+const { default: mongoose } = require("mongoose");
+const { Schema } = mongoose;
+let todoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number
+  }
+});
+
+
+
+module.exports = mongoose.model("Todolist", todoSchema);
